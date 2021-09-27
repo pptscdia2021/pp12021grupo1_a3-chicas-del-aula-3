9a8cb1a53534c1e3d528069e73162a89382d4dda
Jhesic <jess1814@outlook.es>
2021-09-27 18:32:53 -0300

    Comienzo del Proyecto

---
+++ main.py
@@ -0,0 +1,5 @@
+# Se desea comparar valores de cotizaciones entre algunas fuentes
+# de datos del mercado financiero. Desarrollar aplicación en Python
+# tomando datos de página web haciendo “Web Scrapping”, guardado de
+# datos en archivo CSV y presentando los datos en una tabla.
+# Obtener datos vía Yahoo! Finance usando yFinance, o YahooFinance
