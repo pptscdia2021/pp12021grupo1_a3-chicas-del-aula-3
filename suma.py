8884699d29a7d8a72aa5ce17b6da795045a50b2e
Jhesic <73752948+Jhesic@users.noreply.github.com>
2021-09-27 17:49:54 -0300

    Delete suma.py

--- suma.py
+++
@@ -1,9 +0,0 @@
-#from filemodulo import modulo
-
-
-#Prueba modulo
-#print(f"modulo: {modulo(dividendo, divisor)}")
-
-# Suma, 3 parámetros, devuelve la suma de los 3.
-def sumar(a, b,c):
-    return a+b+c
